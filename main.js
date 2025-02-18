9b5ab03464e2fa1b98ebc389175d4f4ffefda571
ploren purba <plorentinpurba1@gmail.com>
2025-02-18 11:18:34 +0700

    pertemuan ke 2

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("hayyy");
+const angka = 1;
+console.log(angka)
+
+const mobil = ["Suzuki","Toyota","Inova"];
+console.log(mobil[0]);
+console.log(mobil.length);
+for(let i =0; mobil.length > i ;i ++){
+    console.log(mobil[i]);
+}
+mobil.push("BMW");
+console.log(mobil)
+
+mobil.pop();
+console.log(mobil)
+
+for(let i = 0; mobil.lenght >= i ; i++){
+    console.log(mobil);
+    mobil.pop();
+    console.log(mobil);
+    if (mobil.lenght ===1){
+        console.log("data sudah habis")
+    };
+};
---
+++ perulangan.js
@@ -0,0 +1,7 @@
+const mobil = ["Suzuki","Toyota","Inova"];
+for(let i = mobil.length ; i > 0 ; i--){
+    console.log(mobil);
+    mobil.pop();
+}
+console.log("data sudah habis");
+    
